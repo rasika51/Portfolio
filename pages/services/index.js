a5e3c3e15208e5acc4852276ac7d5f35062042e1
Rasika Wedaarachchi <wedaarachchirasika@gmail.com>
2024-12-18 01:59:43 +0530

    Add 4 pages
    Contact page
    Service page
    Testimonial page
    Work page

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,40 +1,4 @@
 // icons
-import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-} from "react-icons/rx";
-
-// service data
-export const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-];
 
 
 //  components
@@ -55,16 +19,36 @@ const Services = () => {
         <div className='flex flex-col xl:flex-row gap-x-8 '>
           {/* text */}
           <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-            <h2 className='h2 xl:mt-8'>
-              My services <span className='text-accent'>.</span>
-            </h2>
-            <p className='mb-4 max-w-[400px] mx-auto lg:mx-0 '>
+            <motion.h2 
+              className='h2 xl:mt-8'
+              variants={fadeIn('up', 0.2)} 
+              initial='hidden'
+              animate='show'
+              exit='hidden' 
+            >
+                My services <span className='text-accent'>.</span>
+            </motion.h2>
+            <motion.p 
+              variants={fadeIn('up', 0.4)} 
+              initial='hidden'
+              animate='show'
+              exit='hidden' 
+              className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+            >
               jjjjjjjjjjj jjjjjjjjjjj kkkkkkkkk i yy u u u  tttttttttttttt rrrrrr rrrr rrrr r r rr rrrrrrrrrrrr     rr rrrrrrrrr rrrrrfff f f  fffffffff fffffffff    ffffffffff ffffffff fffffffffff ffffffff
-            </p>
+            </motion.p>
           </div>
 
           {/* slider */}
-          <ServiceSlider />
+          <motion.div 
+            variants={fadeIn('down', 0.6)} 
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='w-full xl:max-w-[65%]'
+          >
+            <ServiceSlider />
+          </motion.div> 
         </div>
       </div>
       <Bulb />
